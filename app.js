3020a02c136b19dd0ec3aaf04d6b9ab797c5df74
byuneunhye0104 <gracebyun06@gmail.com>
2023-01-02 09:12:01 +0900

    2.2

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-console.log("hello")
+console.log("hello");
 
-const a = 10
-const b = "5"
+const a = 10;
+const b = "5";
+const myName = "eunhye";
 
-console.log(a + b)
-console.log(a * b)
-console.log(a / b)
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+//브라우저의 콘솔 창에서 결과를 확인할 수 있음
