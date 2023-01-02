7244db8ab58bd73bfa7c88c1c4b8b7e4c89014d5
byuneunhye0104 <gracebyun06@gmail.com>
2023-01-02 11:50:08 +0900

    #2.4 Booleans

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-console.log("hello");
+let amIFat = false;
+console.log(amIFat);
 
-const a = 10;
-const b = "5";
-const myName = "eunhye";
+amIFat = true;
+console.log(amIFat);
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
+amIFat = null;
+console.log(amIFat);
+
+let someting; //값을 따로 주지 않음
+console.log(someting)//undefined 출력
 
 //브라우저의 콘솔 창에서 결과를 확인할 수 있음
